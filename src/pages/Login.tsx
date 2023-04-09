780c418eeffa7b55a4940c68f64894a31b6edcfb
Surya <123004250@sastra.ac.in>
2023-04-09 16:46:47 +0530

    feat: added appbar and authform components in login and signup page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import { AppBar,AuthForm } from "@/components"
+export default function Login() {
+
+
+    return (<div>
+        <AppBar isLoggedIn={false} />
+        <AuthForm pageName='Login'/>
+    </div>)
+}
