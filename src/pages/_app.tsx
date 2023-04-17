8f19f9c256e9a39d512d8d62995696a72b0e0dbd
Surya <123004250@sastra.ac.in>
2023-04-17 06:02:58 +0530

    feat: added auth context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,26 @@ import {
   Hydrate,
   QueryClient,
   QueryClientProvider,
-} from '@tanstack/react-query'
+} from '@tanstack/react-query';
+import { AuthProvider } from '@/contexts/AuthContext';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient())
-
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        // ✅ globally default to 20 seconds
+        staleTime: 1000 * 20,
+      },
+    },
+  }
+  ))
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
+    </AuthProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { Inter } from 'next/font/google'
-import { Sidebar } from '@/components'
+import { Sidebar ,AppBar} from '@/components'
+import { useAuth } from '@/contexts/AuthContext'
+import { useEffect } from 'react'
+import { useUser } from '@/hooks'
+import { getUser } from '@/utils/userUtils'
+import { CourseCard } from '@/components/CourseCard'
 // import sidebarStyles from '@styles/Sidebar.module.css'
 
 const inter = Inter({
@@ -11,6 +17,28 @@ const inter = Inter({
 })
 
 export default function Home() {
+  const router = useRouter()
+  const { auth,setAuth } = useAuth();
+  const { user, isAuthenticated, token } = auth;
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      const user = getUser();
+      if (user) {
+        setAuth({
+          user: user,
+          isAuthenticated: true,
+          token:localStorage.getItem('token')
+        })
+      }
+    } else {
+      router.push('/Login')
+    }
+  }, [])
+ 
+  if (!isAuthenticated) {
+    return<div>Loading...</div>
+  }
+ 
   return (
     <>
       <Head>
@@ -19,8 +47,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={inter.className}>
+      <main className={inter.className} suppressHydrationWarning={true}>
+        <AppBar isLoggedIn={isAuthenticated} />
         <Sidebar />
+        {user.courses.map((course) => {
+          return <CourseCard course={course.course} key={course.id} />
+        })}
       </main>
     </>
   )
